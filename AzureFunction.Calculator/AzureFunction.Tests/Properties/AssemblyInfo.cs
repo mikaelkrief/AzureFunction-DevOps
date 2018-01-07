62655aafbeea799981117319bc98870c12aaa762
Mikael Krief <krief_mikael@hotmail.com>
2018-01-07 22:10:53 +0100

    add tests

---
+++ AzureFunction.Calculator/AzureFunction.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AzureFunction.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzureFunction.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16d7f5be-6b17-4851-9b15-7f5642ecd5ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AzureFunction.Calculator/AzureFunction.Tests/UnitTestCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureFunction.Tests
+{
+    [TestClass]
+    public class UnitTestCalculator
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            int a = 5;
+            int b = 5;
+
+            int res = AzureFunction.Calculator.Calculator.Add(a, b);
+            Assert.AreEqual(res, 10);
+        }
+
+        [TestMethod]
+        public void TestSubstract()
+        {
+            int a = 15;
+            int b = 10;
+
+            int res = AzureFunction.Calculator.Calculator.Substract(a, b);
+            Assert.AreEqual(res, 5);
+        }
+    }
+}
